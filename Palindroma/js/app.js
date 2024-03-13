89ac13dc5c448352e1353706e048db2165f37ac0
valeriogonini <valeriogonini@gmail.com>
2024-03-13 09:39:07 +0100

    add commit

--- Palindroma/js/app.js
+++ Palindroma/js/app.js
@@ -1,11 +1,9 @@
 //Palidroma
+
 //Chiedere all’utente di inserire una parola
-//Creare una funzione per capire se la parola inserita è palindroma:
-//la funzione deve ritornare true se la parola è palindroma
-//deve ritornare false se la parola non è palindroma
-//alla fine stampate in console un messaggio per dire all’utente il risultato del controllo
 
 const ParolaUtente = prompt("Inserire una parola")
+//Creare una funzione per capire se la parola inserita è palindroma:
 
 const arreyRow = []
 
@@ -22,27 +20,31 @@ for (let j = ParolaUtente.length - 1; j >= 0; j--) {
     arreyRowReverse.push(IndexParolaReverse)
 }
 console.log(arreyRowReverse);
+//la funzione deve ritornare true se la parola è palindroma
+//deve ritornare false se la parola non è palindroma
 
 
 function PalidromaParola(ParolaUtente1, ParolaUtente2) {
     let result = false
     for (let i = 0; i < ParolaUtente.length; i++) {
         if (ParolaUtente1[i] === ParolaUtente2[i]) {
-            
+
             result = true
         }
 
 
 
     }
-    if ( result === true ){
+    //alla fine stampate in console un messaggio per dire all’utente il risultato del controllo
+
+    if (result === true) {
         console.log("La parola è palindroma");
     } else {
         console.log("La parola non è palindroma");
     }
     return result
 }
- const palidromo = PalidromaParola(arreyRow, arreyRowReverse)
- console.log(palidromo);
+const palidromo = PalidromaParola(arreyRow, arreyRowReverse)
+console.log(palidromo);
 
 
--- Pari e diaspari/js/app.js
+++ Pari e diaspari/js/app.js
@@ -2,11 +2,9 @@ console.log("ciao");
 
 
 //Pari e Dispari
+
+
 //L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-//Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-//Sommiamo i due numeri
-//Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-//Dichiariamo chi ha vinto.
 
 const PromptParola = prompt("Scegli tra pari o dispari")
 const PromptNumero = parseInt(prompt("Inserisci un numero da 1 a 5"))
@@ -15,6 +13,7 @@ console.log(PromptParola, PromptNumero);
 
 
 
+//Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 
 if (PromptNumero > 0 && PromptNumero <= 5 &&
     PromptParola === "pari" ||
@@ -28,15 +27,18 @@ if (PromptNumero > 0 && PromptNumero <= 5 &&
 
 const random = RandomNumber()
 console.log("Numero random", random);
+//Sommiamo i due numeri
 
 const sum = PromptNumero + random;
 console.log("Somma dei numeri", sum);
 
+//Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 
 function ParieDispari() {
     const rest = sum % 2
     return rest
 }
+//Dichiariamo chi ha vinto.
 
 if (ParieDispari() === 0 && PromptParola === "pari") {
     console.log("HAI VINTO IL TUO NUMERO è PARI");
